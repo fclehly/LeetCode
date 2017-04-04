a5fdd1cbf4abc93bc5ceeb9c68db965c40ab7764
fwz <ffwzz@outlook.com>
2017-04-04 13:17:21 +0800

    016 ac

--- Solutions/016-3Sum-Closest/3Sum_Closest.py
+++ Solutions/016-3Sum-Closest/3Sum_Closest.py
@@ -7,8 +7,7 @@ class Solution(object):
         """
         ans = 0
         if len(nums) < 3:
-            return []
-        ans = []
+            return 0
         nums.sort()
         ans = nums[0] + nums[1] + nums[2]
         i = 0
@@ -16,22 +15,28 @@ class Solution(object):
             l = i + 1
             r = len(nums) - 1
             while l < r:
-                if nums[l] + nums[r] + nums[i] < 0:
+                if abs(nums[i] + nums[l] + nums[r] - target) < abs(ans - target):
+                    ans = nums[i] + nums[l] + nums[r]
+                if nums[i] + nums[l] + nums[r] - target < 0:
                     l += 1
-                elif nums[l] + nums[r] + nums[i] > 0:
-                    r -= 1
-                else:
-                    ans.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    r -= 1
                     while l < r and nums[l] == nums[l - 1]:
                         l += 1
+                elif nums[i] + nums[l] + nums[r] - target > 0:
+                    r -= 1
                     while l < r and nums[r] == nums[r + 1]:
                         r -= 1
-            while i < len(nums) - 2 and nums[i] == nums[i + 1]:
-                i += 1
+                else:
+                    return target
+                
+                
             i += 1
+            while i < len(nums) and nums[i] == nums[i - 1]:
+                i += 1
+                    
+
+       
         return ans
 
 s = Solution()
 print(s.threeSumClosest([-1, 2, 1, -4], 1))
+print(s.threeSumClosest([0, 0, 0], 1))
